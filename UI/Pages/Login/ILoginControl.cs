78258fd1ce4b452ee36be35d46bd0fa6533c3641
David Gurdak <Dgurdak@thriftbooks.com>
2017-02-10 08:34:31 -0800

    testing github

---
+++ UI/Pages/Login/ILoginControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.UserControlPresenter
+{
+    public interface ILoginPageControl
+    {
+        event EventHandler SuccessfulLogin;
+        event EventHandler<ValidateLoginEventArgs> ValidateLogin;
+
+        void BindToViewModel();
+
+    }
+
+    public class ValidateLoginEventArgs : EventArgs
+    {
+        public int IdPerson { get; }
+        public string Password { get; }
+
+        public ValidateLoginEventArgs(int intIdPerson, string strPassword)
+        {
+            IdPerson = intIdPerson;
+            Password = strPassword;
+        }
+    }
+}
---
+++ UI/UserControlPresenter/ISplitPageControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.UserControlPresenter
+{
+    public interface ISplitPageControl
+    {
+        event EventHandler<PackagesToSplitEventArgs> SubmitPressed;
+        event EventHandler ResetPressed;
+    }
+
+    public class PackagesToSplitEventArgs : EventArgs
+    {
+        public string Contents { get; }
+
+        public PackagesToSplitEventArgs(string strContents)
+        {
+            Contents = strContents;
+        }
+    }
+}
